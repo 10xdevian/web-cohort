1131c7c18c2be717668695e16b0ee646ba0c2172
macOS <vikram.kr.gupta01@gmail.com>
2025-02-28 23:49:52 +0530

    adding some project

---
+++ POS-Resturant/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl underline text-red-400'>Hello world </h1>
+     
+    </div>
+  )
+}
+
+export default App
