28281461d48bcdccc12f4ba653faeb03fd37e3ec
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2025-04-16 08:02:52 +0300

    adding some of projects

---
+++ Week14/a.js
@@ -0,0 +1,26 @@
+function Greet(fullName) {
+    return console.log("Hello" + fullName);
+}
+function sum(a, b) {
+    return console.log(a + b);
+}
+function isLeagl(age) {
+    if (age > 18) {
+        console.log("You are Adult now so you can watch porn");
+    }
+    else {
+        console.log("Bro put Your nunu Inside pants 😂");
+    }
+}
+function delayBro(x, y, z, u) {
+    return x + y + z + u;
+}
+function subTrackt(x, y) {
+    return y - x;
+}
+function GreetBirthday(name) {
+    return console.log("Hello" + name);
+}
+GreetBirthday("Vibhu Gupta JIIIII");
+sum(10, 6);
+Greet("Hello");
---
+++ Week14/a.ts
@@ -0,0 +1,34 @@
+function Greet (fullName :String){
+  return console.log("Hello"+fullName)
+}
+
+function sum (a:number , b:number){
+  return console.log(a+b)
+}
+
+function isLeagl (age:number){
+  if(age > 18){
+    console.log("You are Adult now so you can watch porn")
+  }
+  else{
+    console.log("Bro put Your nunu Inside pants 😂")
+  }
+}
+
+
+function delayBro (x:number, y:number , z:number , u : number ){
+  return x + y + z + u;
+}
+function subTrackt (x:number , y:number){
+  return y - x;
+  
+}
+function GreetBirthday ( name:String ){
+  return console.log("Hello"+name)
+}
+
+GreetBirthday("Vibhu Gupta JIIIII")
+
+sum(10, 6)
+
+Greet("Hello")
