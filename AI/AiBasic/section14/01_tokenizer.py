5da29ee9b4ce452ab53d0f069f69dc38d577e5a9
Vibhu <vikram.kr.gupta01@gmail.com>
2025-09-06 15:24:42 +0530

    added ai

---
+++ AI/AiBasic/section14/01_tokenizer.py
@@ -0,0 +1,18 @@
+import tiktoken
+from openai import OpenAI
+
+# client = OpenAI()
+
+
+# client.chat.completions("")
+
+enc = tiktoken.encoding_for_model("gpt-4o")
+
+text = "hey there! my name is vikram kumar"
+
+tokens = enc.encode(text)
+print("tokens", tokens)
+
+tokens = [48467, 1354, 0, 922, 1308, 382, 81995, 4829, 14289, 277]
+decodeToken = enc.decode(tokens)
+print("DecodeTokens : ", decodeToken)
---
+++ AI/python/section2/01_dataType/dataType.py
@@ -0,0 +1,13 @@
+sugar_Spoon=2
+print(f"Sugar Quantity: {sugar_Spoon}")
+
+
+
+
+
+
+
+
+
+
+
